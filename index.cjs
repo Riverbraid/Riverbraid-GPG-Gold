72c111101a67508fdae8c562a4ae3b66c0557b28
Riverbraid <michael.tilk@gmail.com>
2026-04-14 07:28:33 +0000

    final: stationary alignment & seal

--- index.cjs
+++ index.cjs
@@ -1,3 +1,5 @@
+((env) => { const WHITELIST = ['PATH', 'GPG_TTY', 'HOME', 'USER', 'LANG']; Object.keys(env).forEach(key => { if (!WHITELIST.includes(key)) delete env[key]; }); env.NODE_NO_WARNINGS = '1'; })(process.env);
+
 'use strict';
 const FINGERPRINT = 'D9475D6B717D0E6C8EC84F6D8F86D9F4F2B083A4';
 exports.getStatus = () => ({
